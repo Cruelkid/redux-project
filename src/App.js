9b2f3fa0d283dac73c3c4f498dfb012041048d6f
Cruelkid <sonofammit@gmail.com>
2021-09-23 23:27:32 +0300

    made header and auth form visible, added login/logout functionality via redux

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import { Fragment } from 'react';
 import Counter from './components/Counter';
+import Header from './components/Header';
+import Auth from './components/Auth';
+import { useSelector } from 'react-redux';
 
 function App() {
-    return <Counter />;
+    const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+
+    return (
+        <Fragment>
+            <Header />
+            {!isAuthenticated && <Auth />}
+            <Counter />
+        </Fragment>
+    );
 }
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,37 @@
 import classes from './Header.module.css';
+import { authActions } from '../store/index';
+import { useDispatch, useSelector } from 'react-redux';
 
 const Header = () => {
-  return (
-    <header className={classes.header}>
-      <h1>Redux Auth</h1>
-      <nav>
-        <ul>
-          <li>
-            <a href='/'>My Products</a>
-          </li>
-          <li>
-            <a href='/'>My Sales</a>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  );
+    const dispatch = useDispatch();
+    const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+
+    const logoutHandler = () => {
+        dispatch(authActions.logout());
+    };
+
+    const authItems = (
+        <nav>
+            <ul>
+                <li>
+                    <a href="/">My Products</a>
+                </li>
+                <li>
+                    <a href="/">My Sales</a>
+                </li>
+                <li>
+                    <button onClick={logoutHandler}>Logout</button>
+                </li>
+            </ul>
+        </nav>
+    );
+
+    return (
+        <header className={classes.header}>
+            <h1>Redux Auth</h1>
+            {isAuthenticated && authItems}
+        </header>
+    );
 };
 
 export default Header;
