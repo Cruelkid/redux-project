388664c7a3c0d5078d42d3d2949efb4b10d882a9
Cruelkid <sonofammit@gmail.com>
2021-09-23 23:29:37 +0300

    enabled UserProfile component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Fragment } from 'react';
 import Counter from './components/Counter';
 import Header from './components/Header';
 import Auth from './components/Auth';
+import UserProfile from './components/UserProfile';
 import { useSelector } from 'react-redux';
 
 function App() {
@@ -10,7 +11,7 @@ function App() {
     return (
         <Fragment>
             <Header />
-            {!isAuthenticated && <Auth />}
+            {!isAuthenticated ? <Auth /> : <UserProfile />}
             <Counter />
         </Fragment>
     );
